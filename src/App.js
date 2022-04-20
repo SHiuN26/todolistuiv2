9adfd440a43831c6c9133c019a49c78738a3505c
SHiuN26 <justodme26@gmail.com>
2022-04-20 23:23:08 +0800

    handleswitch

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 import styled from "styled-components";
 import ListAll from "./ListAll";
 // import {Checkbox, Switch} from 'antd';
-import {Switch} from 'antd';
-import 'antd/dist/antd.css';  
+import { Switch } from "antd";
+import "antd/dist/antd.css";
 //---------------------------------------
 const Container = styled.div`
   background-color: #636363;
@@ -92,23 +92,24 @@ const Container = styled.div`
 function App() {
   const [item, setItem] = useState("");
   const [data, setData] = useState([
-    {key:0,value:"Aaaaaaaa",isCheck:false},
-    {key:1,value:"Bbbbbbbbbb",isCheck:false},
-    {key:2,value:"Ccccccccccc",isCheck:false},
-    {key:3,value:"Ddddddddd",isCheck:false},
-    {key:4,value:"Eeeeeeeeeee",isCheck:false}
+    { key: 0, value: "Aaaaaaaa", isCheck: false },
+    { key: 1, value: "Bbbbbbbbbb", isCheck: false },
+    { key: 2, value: "Ccccccccccc", isCheck: false },
+    { key: 3, value: "Ddddddddd", isCheck: false },
+    { key: 4, value: "Eeeeeeeeeee", isCheck: false },
   ]);
+  const [isSwitch, setSwitch] = useState(false);
 
   function addItem(val) {
     if (val === "") {
       return;
     }
-    const addTodo =  {key:data.length+1, value:val , isCheck:false}
+    const addTodo = { key: data.length + 1, value: val, isCheck: false };
     const newData = [...data, addTodo].map((item, index) => {
       return {
-        ...item,
-        key: index
-      }
+        ...item, //待理解
+        key: index, //待理解
+      };
     });
     setData(newData);
     // setItem("");
@@ -116,31 +117,47 @@ function App() {
 
   function handleDelete(index) {
     const newData = [...data];
-    const dataIndex = newData.map(x=>x.key).indexOf(index)
+    const dataIndex = newData.map((x) => x.key).indexOf(index);
     newData.splice(dataIndex, 1);
     const newData1 = newData.map((item, index) => {
       return {
         ...item,
-        key: index
-      }
-    })
+        key: index,
+      };
+    });
     setData(newData1);
   }
 
   function handleCheck(index) {
     const newData = [...data];
     console.log(newData);
-    const dataIndex = newData.map(x=>x.key).indexOf(index);
-    if(newData[dataIndex].isCheck===true){
-      newData[dataIndex].isCheck=false;
+    const dataIndex = newData.map((x) => x.key).indexOf(index);
+    if (newData[dataIndex].isCheck === true) {
+      newData[dataIndex].isCheck = false;
       setData(newData);
-    }else {
-      newData[dataIndex].isCheck=true;
+    } else {
+      newData[dataIndex].isCheck = true;
       setData(newData);
-    }   
-  } 
+    }
+  }
 
-  
+  function handleSwitch(isSwitch) {
+    const newData = [...data];
+    const newData2 = [...data];
+    if (isSwitch === true) {
+      newData.sort((item) => (item.isCheck === true ? 1 : -1));
+      console.log(newData);
+      setData(newData);
+      setSwitch(isSwitch);
+    } else {
+      console.log(newData2);
+      // newData2.sort(function (a, b) {
+      //   return a.index - b.index;
+      // });
+      setData(data);
+      setSwitch(isSwitch);
+    }
+  }
   return (
     <div className="App">
       <Container>
@@ -149,10 +166,14 @@ function App() {
           <span>Get things done,one item add a time</span>
         </div>
         <div className="ListArea">
-          <ListAll data={data} handleDelete={handleDelete} handleCheck={handleCheck} />
+          <ListAll
+            data={data}
+            handleDelete={handleDelete}
+            handleCheck={handleCheck}
+          />
         </div>
         <div className="Footer">
-        {/* <Switch checked={isSwitch} onChange={handleSwitch} /> */}
+          <Switch defaultChecked={isSwitch} onChange={handleSwitch} />
           <p>Add to todo list</p>
           <div className="addArea">
             <input
@@ -166,8 +187,7 @@ function App() {
               onClick={() => {
                 addItem(item);
               }}
-            >
-            </button>
+            ></button>
           </div>
         </div>
       </Container>
